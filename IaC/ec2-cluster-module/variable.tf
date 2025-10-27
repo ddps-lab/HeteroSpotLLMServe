6641b3f284b5422a17ec5222d0cd080b1b4191c4
mhsong <mhsong@kookmin.ac.kr>
2025-10-27 14:59:56 +0900

    add variable.tf

---
+++ IaC/ec2-cluster-module/variable.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {}
+variable "key_name" {}
+variable "instance_type_count" {}
+variable "prefix" {}
+variable "security_group_id" {}
+variable "subnet_id" {}
+variable "admin_instance_profile_name" {}
